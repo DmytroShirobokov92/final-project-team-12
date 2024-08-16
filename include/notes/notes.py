343e6650b7e02b8c6da17b9a68d4c5a4b72965c4
natalia-siladi <godunova.box@gmail.com>
2024-08-16 12:50:45 +0200

    add email

--- include/notes/notes.py
+++ include/notes/notes.py
@@ -85,12 +85,12 @@ def load_notes(filename="notes.pkl"):
         return NoteBook()
 
 
-def save_data(book, filename="./../addressbook.pkl"):
+def save_data(book, filename="addressbook.pkl"):
     with open(filename, "wb") as f:
         pickle.dump(book, f)
 
 
-def load_data(filename="./../addressbook.pkl"):
+def load_data(filename="addressbook.pkl"):
     try:
         with open(filename, "rb") as f:
             return pickle.load(f)
